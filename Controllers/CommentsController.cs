8617f23d3894415a5b024f7357deae54757b8605
Joanna <5525asd9896@gmail.com>
2024-06-02 19:48:41 +0800

    update test

---
+++ Controllers/CommentsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrjFunNowWebApi.Models;
+
+namespace PrjFunNowWebApi.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly FunNowContext _context;
+
+        public CommentsController(FunNowContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult GetComment()
+        {
+            var comment = _context.Comments.ToList();
+            return Ok(comment);  
+        }
+
+
+
+
+        // GET: Comments
+        public async Task<IActionResult> Index()
+        {
+            var funNowContext = _context.Comments.Include(c => c.Hotel).Include(c => c.Member);
+            return View(await funNowContext.ToListAsync());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Hotel)
+                .Include(c => c.Member)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public IActionResult Create()
+        {
+            ViewData["HotelId"] = new SelectList(_context.Hotels, "HotelId", "HotelAddress");
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "Email");
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CommentId,HotelId,MemberId,CreatedAt,CommentTitle,CommentText,CommentStatus,IsReported,UpdatedAt")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotels, "HotelId", "HotelAddress", comment.HotelId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "Email", comment.MemberId);
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotels, "HotelId", "HotelAddress", comment.HotelId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "Email", comment.MemberId);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CommentId,HotelId,MemberId,CreatedAt,CommentTitle,CommentText,CommentStatus,IsReported,UpdatedAt")] Comment comment)
+        {
+            if (id != comment.CommentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.CommentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HotelId"] = new SelectList(_context.Hotels, "HotelId", "HotelAddress", comment.HotelId);
+            ViewData["MemberId"] = new SelectList(_context.Members, "MemberId", "Email", comment.MemberId);
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Hotel)
+                .Include(c => c.Member)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                _context.Comments.Remove(comment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.CommentId == id);
+        }
+    }
+}
