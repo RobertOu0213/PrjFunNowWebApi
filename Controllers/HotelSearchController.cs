ddb38f686e4eb23321042a0b4b038ead52bb7fce
sky <liskyo5470@gmail.com>
2024-06-04 16:32:47 +0800

    add HotelsPagingDTO

--- Controllers/HotelSearchController.cs
+++ Controllers/HotelSearchController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PrjFunNowWebApi.Models;
+using PrjFunNowWebApi.Models.DTO;
 
 namespace PrjFunNowWebApi.Controllers
 {
@@ -46,6 +47,57 @@ namespace PrjFunNowWebApi.Controllers
             return await _context.HotelSearchBoxes.Where(s => s.HotelName.Contains(keyword)).ToListAsync();
         }
 
+        public async Task<ActionResult<HotelSearchDTO>> GetSpotsBySearch(SearchDTO searchDTO)
+        {
+
+
+
+            ////根據分類編號搜尋景點資料
+            //var spots = searchDTO.categoryId == 0 ? _context.SpotImagesSpots : _context.SpotImagesSpots.Where(s => s.CategoryId == searchDTO.categoryId);
+
+            ////根據關鍵字搜尋景點資料(title、desc) 
+            //if (!string.IsNullOrEmpty(searchDTO.keyword))
+            //{
+            //    spots = spots.Where(s => s.SpotTitle.Contains(searchDTO.keyword) || s.SpotDescription.Contains(searchDTO.keyword));
+            //}
+
+            ////排序
+            //switch (searchDTO.sortBy)
+            //{
+            //    case "spotTitle":
+            //        spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.SpotTitle) : spots.OrderByDescending(s => s.SpotTitle);
+            //        break;
+            //    case "categoryId":
+            //        spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.CategoryId) : spots.OrderByDescending(s => s.CategoryId);
+            //        break;
+            //    default:
+            //        spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.SpotId) : spots.OrderByDescending(s => s.SpotId);
+            //        break;
+            //}
+
+            ////總共有多少筆資料
+            //int totalCount = spots.Count();
+            ////每頁要顯示幾筆資料
+            //int pageSize = searchDTO.pageSize ?? 9;   //searchDTO.pageSize ?? 9;
+            ////目前第幾頁
+            //int page = searchDTO.page ?? 1;
+
+            ////計算總共有幾頁
+            //int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
+
+            ////分頁
+            //spots = spots.Skip((page - 1) * pageSize).Take(pageSize);
+
+
+            ////包裝要傳給client端的資料
+            //SpotsPagingDTO spotsPaging = new SpotsPagingDTO();
+            //spotsPaging.TotalCount = totalCount;
+            //spotsPaging.TotalPages = totalPages;
+            //spotsPaging.SpotsResult = await spots.ToListAsync();
+
+
+            //return spotsPaging;
+        }
 
 
         // PUT: api/HotelSearch/5
---
+++ Models/DTO/HotelsPagingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PrjFunNowWebApi.Models.DTO
+{
+    public class HotelsPagingDTO
+    {
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public List<HotelSearchBox>? HotelsResult { get; set; }
+
+    }
+}
