5b719200fa3885edaf264c1a7bac877ab20b1aa3
liskyo <liskyo5470@gmail.com>
2024-06-04 21:25:01 +0800

    add APi Get*2

--- Models/DTO/HotelSearchDTO.cs
+++ Models/DTO/HotelSearchDTO.cs
@@ -3,13 +3,13 @@
     public class HotelSearchDTO
     {
         public string? keyword { get; set; }
-        public int? HotelId { get; set; } = 0;
+        public int? HotelId { get; set; } 
 
-        public int? MemberId { get; set; } = 0;
+        public int? MemberId { get; set; } 
 
         public string? sortBy { get; set; }
-        public string? sortType { get; set; } = "asc";
-        public int? page { get; set; } = 1;
-        public int? pageSize { get; set; } = 9;
+        public string? sortType { get; set; } 
+        public int? page { get; set; } 
+        public int? pageSize { get; set; } 
     }
 }
