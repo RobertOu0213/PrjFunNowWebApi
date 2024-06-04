a239e7866c6e471c3b67bcd4a83b0e1c63865ec3
sky <liskyo5470@gmail.com>
2024-06-04 17:07:24 +0800

    update Bug

--- Controllers/HotelSearchController.cs
+++ Controllers/HotelSearchController.cs
@@ -22,11 +22,11 @@ namespace PrjFunNowWebApi.Controllers
         }
 
         // GET: api/HotelSearch
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetAllHotels()
-        {
-            return await _context.HotelSearchBoxes.ToListAsync();
-        }
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetAllHotels()
+        //{
+        //    return await _context.HotelSearchBoxes.ToListAsync();
+        //}
 
         // GET: api/HotelSearch/5
         [HttpGet("{id}")]
@@ -47,56 +47,56 @@ namespace PrjFunNowWebApi.Controllers
             return await _context.HotelSearchBoxes.Where(s => s.HotelName.Contains(keyword)).ToListAsync();
         }
 
-        [HttpPost]
-        public async Task<ActionResult<HotelsPagingDTO>> GetHotelsBySearch(HotelSearchDTO hotelSearchDTO)
-        {
+        //[HttpPost]
+        //public async Task<ActionResult<HotelsPagingDTO>> GetHotelsBySearch(HotelSearchDTO hotelSearchDTO)
+        //{
 
-            //根據Hotel分類編號搜尋Hotel分類資料 //未修正
-            var Hotels = hotelSearchDTO.HotelId == 0 ? _context.HotelSearchBoxes : _context.HotelSearchBoxes.Where(s => s.HotelId == hotelSearchDTO.HotelId);
+        //    //根據Hotel分類編號搜尋Hotel分類資料 //未修正
+        //    var Hotels = hotelSearchDTO.HotelId == 0 ? _context.HotelSearchBoxes : _context.HotelSearchBoxes.Where(s => s.HotelId == hotelSearchDTO.HotelId);
 
-            //根據關鍵字搜尋景點資料(HotelName、desc) 
-            if (!string.IsNullOrEmpty(hotelSearchDTO.keyword))
-            {
-                Hotels = Hotels.Where(s => s.HotelName.Contains(hotelSearchDTO.keyword) || s.HotelDescription.Contains(hotelSearchDTO.keyword));
-            }
+        //    //根據關鍵字搜尋景點資料(HotelName、desc) 
+        //    if (!string.IsNullOrEmpty(hotelSearchDTO.keyword))
+        //    {
+        //        Hotels = Hotels.Where(s => s.HotelName.Contains(hotelSearchDTO.keyword) || s.HotelDescription.Contains(hotelSearchDTO.keyword));
+        //    }
 
-            //排序
-            switch (hotelSearchDTO.sortBy)
-            {
-                case "HotelName":
-                    Hotels =hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelName) : Hotels.OrderByDescending(s => s.HotelName);
-                    break;
-                case "HotelId":
-                    Hotels =hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelId) : Hotels.OrderByDescending(s => s.HotelId);
-                    break;
-                default:
-                    Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.LevelStar) : Hotels.OrderByDescending(s => s.LevelStar);
-                    break;
-            }
+        //    //排序
+        //    switch (hotelSearchDTO.sortBy)
+        //    {
+        //        case "HotelName":
+        //            Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelName) : Hotels.OrderByDescending(s => s.HotelName);
+        //            break;
+        //        case "HotelId":
+        //            Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelId) : Hotels.OrderByDescending(s => s.HotelId);
+        //            break;
+        //        default:
+        //            Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.LevelStar) : Hotels.OrderByDescending(s => s.LevelStar);
+        //            break;
+        //    }
 
-            //總共有多少筆資料
-            int totalCount = Hotels.Count();
-            //每頁要顯示幾筆資料
-            int pageSize = hotelSearchDTO.pageSize ?? 9;   //searchDTO.pageSize ?? 9;
-            //目前第幾頁
-            int page = hotelSearchDTO.page ?? 1;
+        //    //總共有多少筆資料
+        //    int totalCount = Hotels.Count();
+        //    //每頁要顯示幾筆資料
+        //    int pageSize = hotelSearchDTO.pageSize ?? 9;   //searchDTO.pageSize ?? 9;
+        //    //目前第幾頁
+        //    int page = hotelSearchDTO.page ?? 1;
 
-            //計算總共有幾頁
-            int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
+        //    //計算總共有幾頁
+        //    int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
 
-            //分頁
-            Hotels = Hotels.Skip((page - 1) * pageSize).Take(pageSize);
-
-
-            //包裝要傳給client端的資料
-            HotelsPagingDTO HotelsPaging = new HotelsPagingDTO();
-            HotelsPaging.TotalCount = totalCount;
-            HotelsPaging.TotalPages = totalPages;
-            HotelsPaging.HotelsResult = await Hotels.ToListAsync();
+        //    //分頁
+        //    Hotels = Hotels.Skip((page - 1) * pageSize).Take(pageSize);
 
 
-            return HotelsPaging;
-        }
+        //    //包裝要傳給client端的資料
+        //    HotelsPagingDTO HotelsPaging = new HotelsPagingDTO();
+        //    HotelsPaging.TotalCount = totalCount;
+        //    HotelsPaging.TotalPages = totalPages;
+        //    HotelsPaging.HotelsResult = await Hotels.ToListAsync();
+
+
+        //    return HotelsPaging;
+        //}
 
 
         // PUT: api/HotelSearch/5
