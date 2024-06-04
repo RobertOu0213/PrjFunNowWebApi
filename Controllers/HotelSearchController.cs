d1188b07c71c2c58b98bc2561dd8301470b50bab
sky <liskyo5470@gmail.com>
2024-06-04 17:38:31 +0800

    測試001

--- Controllers/HotelSearchController.cs
+++ Controllers/HotelSearchController.cs
@@ -21,31 +21,31 @@ namespace PrjFunNowWebApi.Controllers
             _context = context;
         }
 
-        // GET: api/HotelSearch
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetAllHotels()
-        //{
-        //    return await _context.HotelSearchBoxes.ToListAsync();
-        //}
+        //GET: api/HotelSearch
+       [HttpGet]
+        public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetAllHotels()
+        {
+            return await _context.HotelSearchBoxes.ToListAsync();
+        }
 
         // GET: api/HotelSearch/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Hotel>> GetHotel(int id)
-        {
-            var hotel = await _context.Hotels.FindAsync(id);
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Hotel>> GetHotel(int id)
+        //{
+        //    var hotel = await _context.Hotels.FindAsync(id);
 
-            if (hotel == null)
-            {
-                return NotFound();
-            }
+        //    if (hotel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return hotel;
-        }
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetHotelsByKey(string keyword)
-        {
-            return await _context.HotelSearchBoxes.Where(s => s.HotelName.Contains(keyword)).ToListAsync();
-        }
+        //    return hotel;
+        //}
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetHotelsByKey(string keyword)
+        //{
+        //    return await _context.HotelSearchBoxes.Where(s => s.HotelName.Contains(keyword)).ToListAsync();
+        //}
 
         //[HttpPost]
         //public async Task<ActionResult<HotelsPagingDTO>> GetHotelsBySearch(HotelSearchDTO hotelSearchDTO)
@@ -99,67 +99,67 @@ namespace PrjFunNowWebApi.Controllers
         //}
 
 
-        // PUT: api/HotelSearch/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutHotel(int id, Hotel hotel)
-        {
-            if (id != hotel.HotelId)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/HotelSearch/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutHotel(int id, Hotel hotel)
+        //{
+        //    if (id != hotel.HotelId)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(hotel).State = EntityState.Modified;
+        //    _context.Entry(hotel).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HotelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!HotelExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/HotelSearch
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
-        {
-            _context.Hotels.Add(hotel);
-            await _context.SaveChangesAsync();
+        //// POST: api/HotelSearch
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Hotel>> PostHotel(Hotel hotel)
+        //{
+        //    _context.Hotels.Add(hotel);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetHotel", new { id = hotel.HotelId }, hotel);
-        }
+        //    return CreatedAtAction("GetHotel", new { id = hotel.HotelId }, hotel);
+        //}
 
-        // DELETE: api/HotelSearch/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteHotel(int id)
-        {
-            var hotel = await _context.Hotels.FindAsync(id);
-            if (hotel == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/HotelSearch/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteHotel(int id)
+        //{
+        //    var hotel = await _context.Hotels.FindAsync(id);
+        //    if (hotel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Hotels.Remove(hotel);
-            await _context.SaveChangesAsync();
+        //    _context.Hotels.Remove(hotel);
+        //    await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        private bool HotelExists(int id)
-        {
-            return _context.Hotels.Any(e => e.HotelId == id);
-        }
+        //private bool HotelExists(int id)
+        //{
+        //    return _context.Hotels.Any(e => e.HotelId == id);
+        //}
     }
 }
