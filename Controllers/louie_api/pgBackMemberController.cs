31adc12180a71476795fc1cc60762a0edbbccd2e
louie <louie@gmail.com>
2024-06-05 11:05:53 +0800

    建立分支

---
+++ Controllers/louie_api/pgBackMemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrjFunNowWebApi.Controllers.louie_api
+{
+    public class pgBackMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
