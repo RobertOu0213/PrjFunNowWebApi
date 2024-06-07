f7039623857a17bf040e5b76e8ceb10c7c0e7964
“Peter” <ezreal1020@gmail.com>
2024-06-07 18:03:44 +0800

    6/11可以抓出房間資料

--- Controllers/BackSideHotelController.cs
+++ Controllers/BackSideHotelController.cs
@@ -20,21 +20,81 @@ namespace PrjFunNowWebApi.Controllers
             _context = context;
         }
 
-        // GET: api/BackSIdeHotel
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+        public async Task<ActionResult> GetHotel()
         {
+            // 建立查詢，將Hotel和Room表進行連接，並選擇所需的字段
+            var query = from hotel in _context.Hotels
+                        select new
+                        {
+                            HotelID = hotel.HotelId,
+                            HotelName = hotel.HotelName,
+                            HotelAddress = hotel.HotelAddress,
+                            HotelPhone = hotel.HotelPhone,
+                            HotelIsActive = hotel.IsActive  // 判斷isActive狀態
+                        };
 
-            //你寫
+            // 執行查詢並將結果轉換為列表
+            var result = await query.ToListAsync();
 
+            // 返回查詢結果
+            return Ok(result);
 
-
-
-
-            return await _context.Hotels.ToListAsync();
         }
 
-    
+        // GET: api/BackSIdeHotel    
+        [HttpGet("rooms/{roomId}")]
+        public async Task<ActionResult> GetHotelByRoomId(int roomId)
+        {
+            // 獲取指定房间ID的房间信息
+            var room = await _context.Rooms
+                .FirstOrDefaultAsync(r => r.RoomId == roomId);
+
+            // 如果房间不存在，返回404错誤
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            // 獲取與房間關聯的飯店信息
+            var hotel = await _context.Hotels
+                .FirstOrDefaultAsync(h => h.HotelId == room.HotelId);
+
+            // 如果飯店不存在，返回404错误
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            // 創建一个包含飯店和房间信息的新對象
+            var selectedHotel = new
+            {
+                HotelId = hotel.HotelId,
+                HotelName = hotel.HotelName,
+                HotelAddress = hotel.HotelAddress,
+                HotelPhone = hotel.HotelPhone,
+                HotelDescription = hotel.HotelDescription,
+
+                // 只包含指定房间的信息
+                Rooms = new[] {
+            new {
+                RoomId = room.RoomId,
+                RoomName = room.RoomName,
+                RoomPrice = room.RoomPrice,
+                Description = room.Description,
+                RoomTypeId = room.RoomTypeId,
+                RoomStatus = room.RoomStatus,
+                RoomSize = room.RoomSize,
+                MaximumOccupancy = room.MaximumOccupancy,
+                MemberId = room.MemberId
+            }
+        }
+            };
+
+            return Ok(selectedHotel);
+        }
+
+
 
 
         // PUT: api/BackSIdeHotel/5
