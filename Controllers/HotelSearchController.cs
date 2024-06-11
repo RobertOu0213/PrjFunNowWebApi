38447ab0605dda4981b8fc993197e2a1fc301002
liskyo <124433904+liskyo@users.noreply.github.com>
2024-06-11 21:27:40 +0800

    去除不必要API

--- Controllers/HotelSearchController.cs
+++ Controllers/HotelSearchController.cs
@@ -17,17 +17,7 @@ namespace PrjFunNowWebApi.Controllers
         {
             _context = context;
         }
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetSpots()
-        {
-            return await _context.HotelSearchBoxes.ToListAsync();
-        }
-        [HttpGet]
-        [Route("search")]
-        public async Task<ActionResult<IEnumerable<HotelSearchBox>>> GetSpots1(string keyword)
-        {
-            return await _context.HotelSearchBoxes.Where(s => s.HotelName.Contains(keyword)).ToListAsync();
-        }
+
 
 
         [HttpPost]
@@ -127,56 +117,6 @@ namespace PrjFunNowWebApi.Controllers
 
 
 
-        [HttpPost]
-        public async Task<ActionResult<HotelsPagingDTO>> GetHotelsBySearch(HotelSearchDTO hotelSearchDTO)
-        {
-
-            //根據Hotel分類編號搜尋Hotel分類資料 //未修正
-            var Hotels = hotelSearchDTO.HotelId == 0 ? _context.HotelSearchBoxes : _context.HotelSearchBoxes.Where(s => s.HotelId == hotelSearchDTO.HotelId);
-
-            //根據關鍵字搜尋景點資料(HotelName、desc) 
-            if (!string.IsNullOrEmpty(hotelSearchDTO.keyword))
-            {
-                Hotels = Hotels.Where(s => s.HotelName.Contains(hotelSearchDTO.keyword) || s.HotelDescription.Contains(hotelSearchDTO.keyword));
-            }
-
-            ////排序
-            //switch (hotelSearchDTO.sortBy)
-            //{
-            //    case "HotelName":
-            //        Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelName) : Hotels.OrderByDescending(s => s.HotelName);
-            //        break;
-            //    case "HotelId":
-            //        Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.HotelId) : Hotels.OrderByDescending(s => s.HotelId);
-            //        break;
-            //    default:
-            //        Hotels = hotelSearchDTO.sortType == "asc" ? Hotels.OrderBy(s => s.LevelStar) : Hotels.OrderByDescending(s => s.LevelStar);
-            //        break;
-            //}
-
-            //總共有多少筆資料
-            int totalCount = Hotels.Count();
-            ////每頁要顯示幾筆資料
-            //int pageSize = hotelSearchDTO.pageSize ?? 9;   //searchDTO.pageSize ?? 9;
-            ////目前第幾頁
-            //int page = hotelSearchDTO.page ?? 1;
-
-            ////計算總共有幾頁
-            //int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
-
-            ////分頁
-            //Hotels = Hotels.Skip((page - 1) * pageSize).Take(pageSize);
-
-
-            //包裝要傳給client端的資料
-            HotelsPagingDTO hotelsPaging = new HotelsPagingDTO();
-            hotelsPaging.TotalCount = totalCount;
-            //hotelsPaging.TotalPages = totalPages;
-            hotelsPaging.HotelsResult = await Hotels.ToListAsync();
-
-
-            return hotelsPaging;
-        }
 
 
 
