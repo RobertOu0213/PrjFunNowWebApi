987d9124fb1b2676e5dd6f52664140fe3aeff60f
“OuCheChun” <starshining12345@gmail.com>
2024-06-19 21:04:23 +0800

    Email Controller 功能

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -56,6 +56,7 @@ namespace PrjFunNowWebApi.Controllers
     <meta charset=""UTF-8"">
     <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
     <title>FunNow樂遊網</title>
+    <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"" crossorigin=""anonymous"">
     <style>
         body {{
             font-family: Arial, sans-serif;
@@ -195,6 +196,7 @@ namespace PrjFunNowWebApi.Controllers
             </a>
         </div>
     </div>
+  <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"" integrity=""sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"" crossorigin=""anonymous""></script>
 </body>
 </html>";
                 // 發送郵件，使用 styledBody 作為郵件內容
