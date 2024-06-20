d138840283457e105cef4ce55b9c885e06bc2dce
ruby <rubyccww@gmail.com>
2024-06-20 15:13:19 +0800

    房東個人資料可以上傳圖片了!

--- Controllers/HostMemberController.cs
+++ Controllers/HostMemberController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PrjFunNowWebApi.Models;
+using PrjFunNowWebApi.Models.DTO;
 
 namespace PrjFunNowWebApi.Controllers
 {
@@ -51,5 +52,99 @@ namespace PrjFunNowWebApi.Controllers
 
             return Ok(cities);
         }
+
+        //根據 cityID 查詢國家
+        [HttpGet("cityDetails")]
+        public async Task<IActionResult> GetCityDetails(int cityId)
+        {
+            var city = await _context.Cities
+                .Include(c => c.Country)
+                .FirstOrDefaultAsync(c => c.CityId == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var cityDetails = new
+            {
+                CityId = city.CityId,
+                CityName = city.CityName,
+                CountryId = city.Country.CountryId,
+                CountryName = city.Country.CountryName,
+            };
+
+            return Ok(cityDetails);
+        }
+
+
+        ////修改會員所有資料
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> HostMemberEdit(int id, HostMemberEditDTO edit)
+        //{
+        //    var member = await _context.Members.FindAsync(id);
+        //    if (member == null)
+        //    {
+        //        return BadRequest("一開始資料庫就沒有這個會員");
+        //    }
+
+        //    member.FirstName = edit.FirstName;
+        //    member.LastName = edit.LastName;
+        //    member.Phone = edit.Phone;
+        //    member.Birthday = edit.Birthday;
+        //    member.CityId = edit.CityId;
+        //    member.MemberAddress = edit.MemberAddress;
+        //    member.Introduction = edit.Introduction;
+
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!MemberExists(id))
+        //        {
+        //            return BadRequest("你在把更新資料存進資料庫時找不到這個會員了");
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return Ok("會員資料修改成功");
+        //}
+
+        //private bool MemberExists(int id)
+        //{
+        //    return _context.Members.Any(e => e.MemberId == id);
+        //}
+
+
+        //public class HostMemberEditDTO
+        //{
+
+        //    public string FirstName { get; set; }
+
+        //    public string LastName { get; set; }
+
+        //    public string Phone { get; set; }
+
+        //    public DateTime? Birthday { get; set; }
+
+        //    public int? CityId { get; set; }
+
+        //    public string MemberAddress { get; set; }
+
+        //    public string Introduction { get; set; }
+
+
+        //}
+
+
+
+       
+
     }
 }
